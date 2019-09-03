b00674dc828dbe93ff09bf246cea938c09098185
Scott Renz <scottrenz@scottrenz.com>
2019-09-03 17:22:13 -0400

    added gets, delete, put

--- server.js
+++ server.js
@@ -1,94 +1,81 @@
-// see a list of pre-populated movies
-// add a movie to the list
-// update movie information
-// remove a movie
-// see only released movies
 
 const express = require('express');
 
 const server = express();
 
 server.use(express.json()); // teaches express to parse JSON body
-let movieId = 5;
-let actorId = 3;
-let actors = [
-  {
-    id: 1,
-    name: 'Elijah Wood',
-    movies: [1, 2],
-  },
-  {
-    id: 2,
-    name: 'Chris Evans',
-    movies: [4],
-  },
-];
-let movies = [
-  {
-    id: 1,
-    name: 'The Fellowship of the Ring',
-    released: true,
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'The Two Towers',
-    released: true,
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: 'The Children of Hurin',
-    released: false,
-    rating: null,
-  },
-  {
-    id: 4,
-    name: 'Avengers Endgame',
-    released: true,
-    rating: 5,
-  },
-];
+let titleId = 0;
+let textId = 0;
 
 // sanity check endpoint
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'up...' });
 });
 
-// test query string: localhost:8000/api/movies?minrating=3
-server.get('/api/movies', (req, res) => {
-  const minRating = req.query.minrating;
 
-  let result = [...movies];
+server.post('/api/posts', (req, res) => {
+    const title = req.body;
+  
+    // add the new id
+     title.id = titleId++;
+    titles.push(title);
+  
+    // return correct http status code for operation
+    res.status(201).json(titles);
+  });
 
-  // if the client provides a minrating, filter the response
-  if (minRating) {
-    result = movies.filter(m => m.rating >= minRating);
-  }
+  server.post('/api/posts/:id/comments', (req, res) => {
+    const text = req.body;
+  
+    // add the new id
+    text.id = textId++;
+    texts.push(text);
+  
+    // return correct http status code for operation
+    res.status(201).json(texts);
+  });
 
-  res.status(200).json(result);
+
+  server.get('/api/posts', (req, res) => {
+    res.send(req);
+
+  res.status(200).json(req);
 });
 
-server.post('/api/movies', (req, res) => {
-  const movie = req.body;
+server.get('/api/posts/:id', (req, res) => {
+    res.send(req);
 
-  // add the new id
-  movie.id = movieId++;
-  movies.push(movie);
-
-  // return correct http status code for operation
-  res.status(201).json(movies);
+  res.status(200).json(req);
 });
 
-server.delete('/api/movies/:id', (req, res) => {
-  const id = req.params.id;
+server.get('/api/posts/:id/comments', (req, res) => {
+    res.send(req);
 
-  movies = movies.filter(m => m.id !== Number(id));
-
-  res.status(200).json(movies);
+  res.status(200).json(req);
 });
 
-// as user of this api, I want and endpoint to see a list of actors, so that I can see all the actors
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+  
+    titles = titles.filter(t => t.id !== Number(id));
+  
+    res.status(200).json(titles);
+  });
+let newTitles = []
+function getTitles(item,ix,arr,id,res) {
+if (arr[ix].id === id)
+{newTitles.push(res)}
+else
+{newTitles.push(item)}
+}
+
+  server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+ titles.map(getTitles)
+    titles = newTitles
+    res.status(200).json(titles);
+  });
+    
 
 // export default server; // ES2015 modules
 // module.exports = { server }; // CommonJS modules (node)
